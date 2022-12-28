40f0c7c9c70bf9f91bdae443688ef672a0a0e3f6
Zariyo <wiktorplu@gmail.com>
2022-12-28 16:11:29 +0100

    delete useless files

--- web_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.start, name='start'),
-    path("shutdown", views.shutdown, name="shutdown"),
-    path("pause", views.pause, name="pause")
-]
